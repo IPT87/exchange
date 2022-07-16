3dbf04ddd8d0b78e6ba96dad043cdf2860ae1edd
Ivaylo <itrifonov87@gmail.com>
2022-07-16 21:50:04 +0200

    Added controllers and Spring annotations.

--- src/main/java/com/optimissa/repositories/MovementRepository.java
+++ src/main/java/com/optimissa/repositories/MovementRepository.java
@@ -1,9 +1,11 @@
 package com.optimissa.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.optimissa.entities.Movement;
 
+@Repository
 public interface MovementRepository extends JpaRepository<Movement, Integer> {
 
 }
